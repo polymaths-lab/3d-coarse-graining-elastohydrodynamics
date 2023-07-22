21f90d2f5203df83e936334f03aa484d09a0b190
pfuchter <93337642+pfuchter@users.noreply.github.com>
2023-07-22 16:47:44 +0100

    added comments

--- get_params_relaxing_filament.m
+++ get_params_relaxing_filament.m
@@ -12,14 +12,18 @@ function [X,params] = get_params_relaxing_filament();
     %Position of filament base in body frame. 
     %xb = x1, separation between xb (body frame) and x1 (segment 1 frame) is zero
     params.b = [0 0 0];
-    params.b(:,:,2) = [0 0 0];
+
     
     X = zeros(6+3*params.N,1);
 
+    %6 + 3N state vector. X(1:3) contains position of base of filament
+    %X(4:6) contains body generator. Generally dX(4:6)/dt = d(X(7:9))/dt.
+    %X(7:end) contains the generators/exponential map for all segments.
     X(1:3) = [0 0 0]; %Position of filament base in lab frame
     X(4:6) = [0 0 0]; %Body frame, irrelevant here because there is no body
-    X(8:3:end) = linspace(0, pi/2, params.N); %bend filament in plane
 
-%     X = [X;X];
-%     X(1:3) = [-1 -1 -1];
+    X(7:3:end) = 0.0 %first component of generators
+    X(8:3:end) = linspace(0, pi/2, params.N); %second component of generators, here we bend filament in plane
+    X(9:3:end) = 0.0 %third component of generators
+
 end
