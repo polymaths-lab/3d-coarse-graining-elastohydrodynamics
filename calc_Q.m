ce721a5f78dac8e0a43839bb14c968f18efe51a8
pfuchter <p.fuchter@bristol.ac.uk>
2024-03-13 16:46:30 +0000

    Fixed n > 1 bug

--- calc_Q.m
+++ calc_Q.m
@@ -9,9 +9,10 @@ q3 = X(3+4:4:end-0);
 d1 = [q0.*q0 + q1.*q1 - q2.*q2 - q3.*q3, 2*(q2.*q1 + q0.*q3), 2*(q3.*q1-q0.*q2)];
 d2 = [2*(q1.*q2-q0.*q3), q0.*q0-q1.*q1+q2.*q2-q3.*q3, 2*(q3.*q2+q0.*q1)];
 d3 = [2*(q1.*q3+q0.*q2), 2*(q2.*q3-q0.*q1), q0.*q0-q1.*q1-q2.*q2+q3.*q3];
-
-% N = size(d3, 1);
-
+d1=load("d1.txt");
+d2=load("d2.txt");
+d3=load("d3.txt");
+% d3(3,1) = 400
 a = 1/(2*N*n);
 
 Q = zeros(6*(Nbody+(Nfil*N*n)), 6+3*N*Nfil);
@@ -46,18 +47,18 @@ for fil = 1:Nfil
         
         Q(3*(Nbody+(Nfil*N*n))+3*(sn-1)+1:3*(Nbody+(Nfil*N*n))+3*sn,3+3+3*(seg_n-Nbody-1)+1:3+3+3*(seg_n-Nbody)) = eye(3); %omega
     end
-    for i = 2:N
-        
+    for i = 2:N        
         seg_n = Nbody + (fil-1)*N+i;
-        d3i = d3(1+seg_n-Nbody, :);
-        d3cross = [0 -d3i(3) d3i(2); d3i(3) 0 -d3i(1); -d3i(2) d3i(1) 0];
+   
         for k = 1:n
+            d3i = d3(1+seg_n-Nbody, :);
+            d3cross = [0 -d3i(3) d3i(2); d3i(3) 0 -d3i(1); -d3i(2) d3i(1) 0];
             sn = Nbody+(fil-1)*N*n+(i-1)*n+k;
             Q(3*(sn-1)+1:3*sn, 1:3) = eye(3);
             Q(3*(sn-1)+1:3*sn, 4:6) = -rcross;
             
             %set rows = to rows from previous sphere
-            Q(3*(sn-1)+1:3*sn, 3+3+3*(fil-1)*N*n+1:3+3+3*fil*N) = Q(3*(sn-1-1)+1:3*(sn-1), 3+3+3*(fil-1)*N*n+1:3+3+3*fil*N);
+            Q(3*(sn-1)+1:3*sn, :) = Q(3*(sn-1-1)+1:3*(sn-1), :);
             
             Q(3*(sn-1)+1:3*sn, 3+3+(seg_n-Nbody-1)*3+1:3+3+(seg_n-Nbody)*3) = -(2*k*a-a)*d3cross;
             
